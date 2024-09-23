f3f88c320553b77fae156b5cab30ea388d59e404
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-09-23 13:45:21 -0300

    Comienzo del proyecto!

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,18 @@
+/*
+    Javascript Exercises For Beginners
+    ==================================
+
+    Beginner Exercises: Part 1
+    --------------------------
+
+    Exercise 1:
+
+    Write a Javascript Program to check 2 numbers and 
+    return "true" if one of the numbers is 100 or if the sum of the 2 numbers is 100.
+*/
+
+const isEqualTo100 = (a, b) => a === 100 || b
+=== 100;
+
+console.log(isEqualTo100(100,0)); // PRINT THE SUM OF THIS 2 NUMBERS EQUALS TO 100 AND RETURNS TRUE (a,b).
+console.log(isEqualTo100(0,100)); 
