522b9f5fcfa1c758922bb8688f264b1ba71d0f69
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-11-07 13:14:48 -0300

    Ejercicios concluidos con éxito al 100%.

--- ArchivosEjecutables/script.js
+++ ArchivosEjecutables/script.js
@@ -84,3 +84,19 @@ const formatDate = (date = new Date()) => {
 }
 
 console.log(formatDate());
+
+/* Exercise 5:
+   -----------
+
+   Write a JavaScript program to create a new
+   string adding "New!" in front oof a given string.
+
+   If the given string begins with "New!" already
+   then return the original string.
+
+*/
+
+const addNew = (str) =>
+    str.indexOf('New!') === 0 ? str : `New! ${str}`;
+
+console.log(addNew('New! Offers'));
